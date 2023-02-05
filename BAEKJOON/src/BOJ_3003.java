d2af0e10836245b0076651bf6cce964e88b7edf6
kky156 <kky156@naver.com>
2023-02-05 23:54:36 +0900

    upload

---
+++ BAEKJOON/src/BOJ_3003.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BOJ_3003 {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	
+	int[] arr= {1,1,2,2,2,8};
+	
+	for(int i=0; i<arr.length;i++) {
+		int temp=sc.nextInt();
+		
+		System.out.print(arr[i]-temp+" ");
+	}
+	
+}
+}
