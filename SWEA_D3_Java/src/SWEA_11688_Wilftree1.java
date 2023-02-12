ba7e12ea81e1673382590e5946bef94455a5ed4b
kky156 <kky156@naver.com>
2023-02-12 11:16:12 +0900

    230212

--- SWEA_D3_Java/src/SWEA_11688_Wilftree1.java
+++ SWEA_D3_Java/src/SWEA_11688_Wilftree1.java
@@ -1,4 +1,46 @@
+import java.util.Scanner;
 
 public class SWEA_11688_Wilftree1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+
+		for (int i = 1; i <= T; i++) {
+
+			String s = sc.next();
+			int u = 1; // u/d
+			int d = 1;
+
+			for (int j = 0; j < s.length(); j++) {
+				if (s.charAt(j) == 'L') {
+					d += u;
+				} else if (s.charAt(j) == 'R') {
+					u += d;
+				}
+			}
+			int a = u;
+			int b = d;
+
+			while (true) {
+
+				if (a > b) {
+					a -= b;
+
+				} else if (a < b) {
+					b -= a;
+				}
+
+				if (a == b) {
+					break;
+				}
+			}
+
+			u /= a;
+			d /= a;
+			System.out.printf("#%d %d %d\n", i, u, d);
+
+		}
+	}
 
 }
