ab75e60df2e77389a2b8daf3541119d142ab9b9b
Yejin Kim <123815209+Kim-Yejinn@users.noreply.github.com>
2023-12-18 14:04:52 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 13224 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -2,32 +2,35 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-		int n = sc.nextInt(); // 계단수
+        int N = sc.nextInt();
 
-		int[] arr = new int[n + 1];
-		int[] memo = new int[n + 1];
+        int[] score = new int[N+1];
+        for(int i=1; i<=N; i++){
+            score[i] = sc.nextInt();
+        }
+        int[] dp = new int[N+1];
+        dp[1] = score[1];
+        if(N>=2){
+            dp[2] = dp[1]+score[2];
+        }
 
-		for (int i = 1; i <= n; i++) {
-			arr[i] = sc.nextInt();
-		}
+        for(int i=3; i<=N; i++){
+            int num1 = dp[i-2];
+            int num2 = score[i-1]+dp[i-3];
 
-		memo[1] = arr[1];
-		if (n >= 2) {
-			memo[2] = arr[2] + arr[1];
-		}
-		// 1. 지금 위치가 선택되었을 경우 -> 직전이 선택 안됨 => i-2의 최대값에 현재 추가
-		// 2. 지금 위치가 선택되었을 경우 -> 직전도 선택 됨 => i-3의 최대값에 i-1, i값 추가
+            int max= 0;
+            if(num1>num2){
+                max = num1;
+            }else{
+                max = num2;
+            }
 
-		for (int i = 3; i <= n; i++) {
-			int a = memo[i - 2] + arr[i];
-			int b = memo[i - 3] + arr[i] + arr[i - 1];
+            dp[i] = max+score[i];
+        }
+        System.out.println(dp[N]);
 
-			memo[i] = Math.max(a, b);
-		}
-		System.out.println(memo[n]);
-
-	}
+    }
 }
