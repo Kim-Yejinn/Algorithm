606106254afb5b2aa63cc0a3a697b76688295563
Yejin Kim <123815209+Kim-Yejinn@users.noreply.github.com>
2024-01-16 10:22:05 +0900

    [Silver II] Title: 랜선 자르기, Time: 148 ms, Memory: 15376 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,42 +1,44 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int K = sc.nextInt();
-        int N = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        long[] list = new long[K];
 
-        int[] arr = new int[K];
+//        long left = 0;
+//        long right = 0;
 
-        int max = 0;
-
-        for(int i=0; i<K; i++){
-            arr[i] = sc.nextInt();
-            if(arr[i]>max){
-                max = arr[i];
-            }
+        for (int i=0; i<K; i++) {
+            list[i] = Integer.parseInt(br.readLine());
         }
+        Arrays.sort(list);
 
         long left = 1;
-        long right = max;
-        long result = 0;
+        long right = list[K-1]+1;
 
-        while(left<=right){
+
+        while (left < right) {
             long mid = (left+right)/2;
+//            System.out.print(left+" "+mid+" "+right);
 
-            long sum = 0;
-            for(int i=0; i<K; i++){
-                sum += arr[i]/mid;
+            long cnt = 0;
+            for (int i=0; i<K; i++) {
+                cnt += (list[i]/mid);
             }
+//            System.out.println(" // "+cnt);
 
-            if(sum>=N){
-                result = mid;
+            if (cnt < N) { // 더 잘게 잘랐음.. 길이 늘려도됨
+                right = mid;
+            } else {
                 left = mid+1;
-            }else{
-                right = mid-1;
             }
         }
-        System.out.println(result);
+
+        System.out.println(left-1);
     }
 }
