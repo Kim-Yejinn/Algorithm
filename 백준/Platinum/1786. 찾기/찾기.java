d6ca3268450d873515a0b54f8ca514eb6e0e1b14
Yejin Kim <123815209+Kim-Yejinn@users.noreply.github.com>
2024-02-12 14:56:36 +0900

    [Platinum V] Title: 찾기, Time: 556 ms, Memory: 91696 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.java
+++ 백준/Platinum/1786. 찾기/찾기.java
@@ -2,66 +2,63 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+    static int[] pi;
+    static List<Integer> list;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String T = br.readLine();
-        String P = br.readLine();
+        String str = br.readLine();
+        String text = br.readLine();
 
-        if(T.length() < P.length()){
-            System.out.println(0);
-            return;
-        }
+        pi = new int[text.length()];
+        list = new ArrayList<>();
 
-        // 서로소이고 M은 소수로 하는 것이 해시 충돌 확률을 줄일 수 있다.
-        // M은 보통 10억 정도..
-        int C = 31;
-        int M = 1_000_000_007;
-
-        // P의 해시 함수 미리 구하자
-        // T도 처음은 구해두자
-        long ph = 0;
-        long th = 0;
-        long power = 1;
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i=0; i<P.length(); i++){
-            int t1 = P.charAt(P.length()-i-1);
-            int t2 = T.charAt(P.length()-i-1);
-            // 앞 부터 넣은 것이니..
-            ph = (ph + t1 * power) % M;
-            th = (th + t2 * power) % M;
-
-            power = (power * C) % M;
-        }
-        if(th == ph){
-            list.add(1);
-        }
-
-        // T를 돌면서 짜르는데 같은지 확인한다.
-        for(int i=1; i<=T.length() - P.length(); i++){
-            int t1 = T.charAt(i-1); // 지울 것
-            int t2 = T.charAt(i+P.length()-1);
-            // 곱하고
-            th = (th * C) % M;
-            // 맨앞을 지우고
-            th = th + M - (t1 * power) % M;
-            // 새로운 값 더한다.
-            th += t2;
-
-            th %= M;
-            if(th == ph){
-                // 찾으면
-                list.add(i+1);
-            }
-        }
+        kmp(str, text);
         System.out.println(list.size());
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<list.size(); i++){
             sb.append(list.get(i)).append(" ");
         }
         System.out.println(sb);
+    }
 
+    public static void prefix(String text){
+        int j = 0;
+
+        for(int i=1; i<text.length(); i++){
+            while (j>0 && (text.charAt(i) != text.charAt(j))){
+                j = pi[j-1];
+            }
+
+            if(text.charAt(i) == text.charAt(j)){
+                j++;
+            }
+            pi[i] = j;
+        }
+    }
+
+    public static void kmp(String str, String text){
+        prefix(text);
+        int j =0;
+
+        for(int i = 0; i<str.length(); i++){
+            while(j>0 && text.charAt(j) != str.charAt(i)){
+                j = pi[j-1];
+            }
+
+            if(str.charAt(i) == text.charAt(j)){
+                if(j == text.length()-1){
+                    // 같으면
+                    list.add(i - text.length() + 2);
+                    j = pi[j];
+                }else{
+                    j++;
+                }
+            }
+        }
     }
 }
