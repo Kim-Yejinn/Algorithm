bff39aea837049ba33420ad7ffaf0264c45ee147
Yejin Kim <123815209+Kim-Yejinn@users.noreply.github.com>
2024-03-31 14:43:28 +0900

    [Gold IV] Title: 게임, Time: 116 ms, Memory: 12820 KB -BaekjoonHub

--- 백준/Gold/28423. 게임/게임.java
+++ 백준/Gold/28423. 게임/게임.java
@@ -4,12 +4,27 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[] g;
+    static int[] f;
+    static boolean[] visit;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int L = Integer.parseInt(st.nextToken());
         int R = Integer.parseInt(st.nextToken());
+
+        g = new int[100001];
+        f = new int[100001];
+        visit = new boolean[100001];
+
+        for(int i=1; i<=100_000; i++){
+            f[i] = findF(i);
+            if(f[i] == i){
+                g[i] = 1;
+            }
+        }
 
         int ans = 0;
         for(int i=L; i<=R; i++){
@@ -18,31 +33,43 @@ public class Main {
         System.out.println(ans);
     }
 
-    public static int findG(int num){
-        int temp = num;
-        boolean[] check = new boolean[100_001];
+    public static int findF(int num){
+        int A = findA(num);
+        int B = findB(num);
 
-        while (temp > 0){
-            int A = findA(temp);
-            int B = findB(temp);
+        int S = findSize(B);
+        if(S == 1){
+            S = 10;
+        }
+        return A*S + B;
+    }
 
-            int S = findSize(B);
-            if(S == 1){
-                S = 10;
-            }
-            int cal = A*S + B;
+    public static int findG(int idx){
+        if(g[idx] == 1){
+            return 1;
+        }
+        if(g[idx] == -1){
+            return -1;
+        }
+        if(f[idx] > 100000){
+            return -1;
+        }
 
-            if(cal > 100_000){
-                return -1;
-            }
-            if(cal == temp){
-                return 1;
-            }
-            if(check[cal]){
-                return 0;
-            }
-            check[cal] = true;
-            temp = cal;
+        if(visit[idx]){
+            return 0;
+        }
+
+        visit[idx] = true;
+
+        int t = findG(f[idx]);
+
+        if(t == 1){
+            g[idx] = 1;
+            return 1;
+        }
+        if(t == -1){
+            g[idx] = -1;
+            return -1;
         }
         return 0;
 
