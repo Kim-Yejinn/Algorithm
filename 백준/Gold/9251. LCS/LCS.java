caf2c1782b05ad4926f316bb7149153149702c67
Yejin Kim <123815209+Kim-Yejinn@users.noreply.github.com>
2024-04-17 14:32:37 +0900

    [Gold V] Title: LCS, Time: 120 ms, Memory: 16024 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        int[][] dp = new int[str1.length()+1][str2.length()+1];
+
+        for(int r=1; r<=str1.length(); r++){
+            char a = str1.charAt(r-1);
+            for(int c=1; c<=str2.length(); c++){
+                char b = str2.charAt(c-1);
+                if(a==b){
+                    dp[r][c] = dp[r-1][c-1] + 1;
+                }else{
+                    dp[r][c] = Math.max(dp[r-1][c], dp[r][c-1]);
+                }
+            }
+        }
+
+        System.out.println(dp[str1.length()][str2.length()]);
+    }
+}
