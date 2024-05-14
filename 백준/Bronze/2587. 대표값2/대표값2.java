80186dde5c7a785ce1e727d0188e3db6d8b01a0b
Yejin Kim <123815209+Kim-Yejinn@users.noreply.github.com>
2024-05-14 20:05:56 +0900

    [Bronze II] Title: 대표값2, Time: 80 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> list = new ArrayList<>();
+
+        int sum = 0;
+        for(int i=0; i<5; i++){
+            int t = Integer.parseInt(br.readLine());
+            sum += t;
+            list.add(t);
+        }
+        Collections.sort(list);
+        System.out.println(sum/5);
+        System.out.println(list.get(2));
+    }
+}
