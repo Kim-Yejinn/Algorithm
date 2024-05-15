787ab8bf2e32595c8fc9332c4f7f7f83a7c18a1a
Yejin Kim <123815209+Kim-Yejinn@users.noreply.github.com>
2024-05-15 21:15:08 +0900

    [Silver V] Title: 귀찮음, Time: 76 ms, Memory: 11492 KB, Score: 4 point -BaekjoonHub

---
+++ 백준/Silver/16208. 귀찮음/귀찮음.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        List<Integer> list = new ArrayList<>();
+        int sum = 0;
+        for(int i=0; i<N; i++){
+            int t = Integer.parseInt(st.nextToken());
+            list.add(t);
+            sum += t;
+        }
+
+        Collections.sort(list);
+
+        int ans = 0;
+        for(int i=0; i<N-1; i++){
+            int t = list.get(i);
+            sum -= t;
+            ans += sum * t;
+        }
+        System.out.println(ans);
+    }
+}
